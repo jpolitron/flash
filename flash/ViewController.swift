895b841d91537f30a821b1df804c046a698fda83
Jessie Politron <jessicapolitron@student.hartnel.edu>
2018-10-15 17:58:59 -0700

    Initial Commit

---
+++ flash/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flash
+//
+//  Created by Jessie Politron on 10/15/18.
+//  Copyright © 2018 Jessie Politron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
